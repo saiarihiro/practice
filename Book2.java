def22c67392967177fd00f9b7ccb05b6484f95d2
saiarihiro <ari19778210@gmail.com>
2024-10-17 14:19:40 +0900

    Create Book2.java

---
+++ Book2.java
@@ -0,0 +1,9 @@
+public class Book extends TangibleAsset{
+private String is
+public Book(String name,int price, String color,String is){
+	super(name,price,color);
+	this.is=is;
+	{
+		public String getIs(){return this.is;}
+
+}
